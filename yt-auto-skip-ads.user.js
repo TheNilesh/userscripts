c867cba3588748d35c89792cddd07fe3d5e629a3
Nilesh Akhade <TheNilesh@users.noreply.github.com>
2023-12-06 13:25:22 +0530

    Update button class

--- yt-auto-skip-ads.user.js
+++ yt-auto-skip-ads.user.js
@@ -4,7 +4,7 @@
 // @version      0.2
 // @description  Detects Skip Ad button on youtube video, and clicks it automatically.
 // @author       You
-// @match        https://www.youtube.com/watch*
+// @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
 // @grant        none
 // ==/UserScript==
@@ -39,7 +39,8 @@
         });
     }
     // ytp-ad-skip-button ytp-button
-    waitForElm('.ytp-ad-skip-button').then((elm) => {
+    // ytp-ad-skip-button-modern ytp-button
+    waitForElm('.ytp-ad-skip-button-modern').then((elm) => {
         console.log('This promise will never resolve');
     });
 })();
